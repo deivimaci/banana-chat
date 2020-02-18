596b65338e9550ca8a66a18482ca0052e9a7e025
deivimaci <maciulskisdeividas@gmail.com>
2020-02-18 13:37:28 +0200

    Updates

---
+++ precache-manifest.bf5b5e0e1b9269cbcd7a91e229cead9e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7a5d69ff51a9ffa88f84e64b0d69fa",
+    "url": "/banana-chat/index.html"
+  },
+  {
+    "revision": "2dea3d2f763b5fa47a3e",
+    "url": "/banana-chat/static/css/main.f9d9c408.chunk.css"
+  },
+  {
+    "revision": "74d0be3bb92b21724af9",
+    "url": "/banana-chat/static/js/2.c2028af6.chunk.js"
+  },
+  {
+    "revision": "d9bee4e26266dab8796c9ed1573a3e49",
+    "url": "/banana-chat/static/js/2.c2028af6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dea3d2f763b5fa47a3e",
+    "url": "/banana-chat/static/js/main.0386151e.chunk.js"
+  },
+  {
+    "revision": "3107eccb5e6ee124b50b",
+    "url": "/banana-chat/static/js/runtime-main.8f362773.js"
+  },
+  {
+    "revision": "a2a988bfda9452c377e209b9cd7c3e9d",
+    "url": "/banana-chat/static/media/background.a2a988bf.png"
+  },
+  {
+    "revision": "df6d4afe4f63c6b8c407b504957376c1",
+    "url": "/banana-chat/static/media/banana.df6d4afe.svg"
+  },
+  {
+    "revision": "b01722228d13cee629f72f75f685f037",
+    "url": "/banana-chat/static/media/remove.b0172222.svg"
+  }
+]);
